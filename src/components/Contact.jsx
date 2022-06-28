7953a67927dfdaba73a49067897d5a61bbacbdcf
Lengrand Lucas <lucaslengranddev@gmail.Com>
2022-06-28 10:16:13 +0200

    Formulaire de contact
    mise en page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,50 +19,50 @@ export default function Contact() {
           <div className=" row g-0 mb-5">
             <div className="m-0 g-0 p-0">
               <div className="row p-3 align-items-center">
-              <div className=" col-2 text-center contact--color">
-                <div className="contact__icon col-2 text-center contact--color">
+              <div className=" text-center contact--color">
+                <div className="contact__icon col-12 text-center contact--color">
                   <PersonPinIcon />
                 </div>
                 <span className="p-0 col-9 contact--color"> 24 rue de verville, 91680 Bruyères-le-Châtel</span>
               </div>
               </div>
               <div className="row p-3 align-items-center">
-                <div className=" col-2 text-center contact--color">
+                <div className="text-center contact--color">
                   <EmailIcon />
-                </div>
                 <a 
-                className="nav-link underline__headerLinks nav_text--color contact--color contact__content__text p-0" 
+                className="nav-link underline__headerLinks nav__text--color contact--color contact__content__text p-0" 
                 href="mailto:lucaslengranddev@gmail.com">
                   lucaslengranddev@gmail.com
                 </a>
               </div>
+              </div>
               <div className="row p-3 align-items-center">
-                <div className="contact__icon col-2 text-center contact--color">
+                <div className="contact__icon text-center contact--color">
                   <PhoneIcon />
-                </div>
                 <a className="nav-link underline__headerLinks nav__text--color contact--color p-0 contact__content__text" href="tel:+33667007416">
                   +336 67 00 74 16
                 </a>
               </div>
+              </div>
               <div className="row p-3 align-items-center">
-                <div className="contact__icon col-2 text-center contact--color">
+                <div className="contact__icon text-center contact--color">
                   <EventAvailableIcon />
-                </div>
-                <span className="p-0 col contact--color">
+                <span className="nav-link underline__headerLinks nav__text--color contact--color p-0 contact__content__text">
                   <time dateTime="1996-08-24">24/08/1996</time>
                 </span>
               </div>
-              <div className="row p-3 align-items-center">
-              <div className="contact__icon col-2 text-center contact--color">
-                <GitHubIcon />
               </div>
+              <div className="row p-3 align-items-center">
+              <div className="contact__icon text-center contact--color">
+                <GitHubIcon />
               <a className="nav-link underline__headerLinks nav__text--color contact--color contact__content__text p-0" href="https://github.com/Melliaganz" target="_blank" rel="noreferrer"> Melliaganz </a>.
               </div>
+              </div>
               <div className="row p-3 align-items-center">
-                <div className=" contact__icon col-2 text-center contact--color">
+                <div className=" contact__icon text-center contact--color">
                   <LinkedInIcon />
-                </div>
                 <a className="nav-link underline__headerLinks nav__text--color contact--color contact__content__text p-0" href="http://www.linkedin.com/in/LucasLengrand">Lucas Lengrand</a>
+              </div>
               </div>
             </div>
           </div>
