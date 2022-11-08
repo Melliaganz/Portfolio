2c8d6e718817cdd981c95e1be101291287d25516
Lucas Lengrand <lucaslengranddev@gmail.com>
2022-11-08 13:45:43 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.css';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import CssBaseline from '@mui/material/CssBaseline';
 import About from './components/About';
 import Header from './components/Header'
 import Contact from './components/Contact';
@@ -13,21 +10,8 @@ import ScrollupButton from './components/ScrollupButton';
 import Socials from './components/Socials';
 
 function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? 'dark' : 'light',
-        },
-      }),
-    [prefersDarkMode],
-  );
   return (
     <main className="main">
-      <ThemeProvider theme={theme}>
-      <CssBaseline />
       <Header />
       <ScrollupButton />
       <Socials />
@@ -36,7 +20,6 @@ function App() {
       <Portfolio />
       <Contact />
       <Footer />
-      </ThemeProvider>
     </main>
   );
 }
