7edb94e759841fc678dd663486537bb51584f2eb
Melliaganz <lucaslengranddev@gmail.com>
2025-12-08 11:34:33 +0100

    mise a jour recaptcha

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, { lazy, Suspense, useState, useMemo, useEffect } from 'react';
-import './App.css';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, ScopedCssBaseline } from '@mui/material';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import React, { lazy, Suspense, useState, useMemo, useEffect } from "react";
+import "./App.css";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline, ScopedCssBaseline } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Lucas from "./img/Portrait.webp";
-import BotDiscord from './components/BotDiscord';
+import BotDiscord from "./components/BotDiscord";
 
-const About = lazy(() => import('./components/About'));
-const Header = lazy(() => import('./components/Header'));
-const Contact = lazy(() => import('./components/Contact'));
-const Formation = lazy(() => import('./components/Formation'));
-const Portfolio = lazy(() => import('./components/Portfolio'));
-const Footer = lazy(() => import('./components/Footer'));
-const ScrollupButton = lazy(() => import('./components/ScrollupButton'));
-const Socials = lazy(() => import('./components/Socials'));
+const About = lazy(() => import("./components/About"));
+const Header = lazy(() => import("./components/Header"));
+const Contact = lazy(() => import("./components/Contact.tsx"));
+const Formation = lazy(() => import("./components/Formation"));
+const Portfolio = lazy(() => import("./components/Portfolio"));
+const Footer = lazy(() => import("./components/Footer"));
+const ScrollupButton = lazy(() => import("./components/ScrollupButton"));
+const Socials = lazy(() => import("./components/Socials"));
 
 const useTheme = () => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   return useMemo(
     () =>
       createTheme({
         palette: {
-          mode: prefersDarkMode ? 'dark' : 'light',
+          mode: prefersDarkMode ? "dark" : "light",
         },
       }),
-    [prefersDarkMode],
+    [prefersDarkMode]
   );
 };
 
@@ -34,43 +35,49 @@ const useLoadingState = () => {
   useEffect(() => {
     const loadingTimeout = setTimeout(() => {
       setIsLoading(false);
-    }, 1000); 
+    }, 1000);
 
     return () => clearTimeout(loadingTimeout);
   }, []);
 
   return { isLoading };
 };
+const recaptchaSiteKey = process.env.REACT_APP_RECAPTCHA_SITE_KEY || "";
 
 function App() {
   const theme = useTheme();
   const { isLoading } = useLoadingState();
 
   return (
-    <main className="main">
-      <ThemeProvider theme={theme}>
-        <CssBaseline enableColorScheme />
-        <ScopedCssBaseline enableColorScheme>
-          {isLoading ? (
-            <div className='loader'>
-              <img className="spinner" src={Lucas} alt="Lucas Lengrand" />
-            </div>
-          ) : (
-            <Suspense>
-              <Header />
-              <ScrollupButton />
-              <Socials />
-              <About />
-              <Portfolio />
-              <BotDiscord/>
-              <Formation />
-              <Contact />
-              <Footer />
-            </Suspense>
-          )}
-        </ScopedCssBaseline>
-      </ThemeProvider>
-    </main>
+    <GoogleReCaptchaProvider
+      reCaptchaKey={recaptchaSiteKey}
+      useRecaptchaNet={true}
+    >
+      <main className="main">
+        <ThemeProvider theme={theme}>
+          <CssBaseline enableColorScheme />
+          <ScopedCssBaseline enableColorScheme>
+            {isLoading ? (
+              <div className="loader">
+                <img className="spinner" src={Lucas} alt="Lucas Lengrand" />
+              </div>
+            ) : (
+              <Suspense>
+                <Header />
+                <ScrollupButton />
+                <Socials />
+                <About />
+                <Portfolio />
+                <BotDiscord />
+                <Formation />
+                <Contact />
+                <Footer />
+              </Suspense>
+            )}
+          </ScopedCssBaseline>
+        </ThemeProvider>
+      </main>
+    </GoogleReCaptchaProvider>
   );
 }
 
---
+++ src/components/Contact.tsx
@@ -0,0 +1,232 @@
+import React, { FormEvent, useState } from "react";
+import EmailIcon from "@mui/icons-material/Email";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import SendIcon from "@mui/icons-material/Send";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+
+export default function Contact() {
+  const formSparkUrl = `https://submit-form.com/zDkyCJXc`;
+
+  const [submitting, setSubmitting] = useState(false);
+
+  const { executeRecaptcha } = useGoogleReCaptcha();
+const submitForm = async (event: FormEvent) => {
+    event.preventDefault(); // Assurez-vous d'empêcher le comportement par défaut de la soumission.
+
+    if (!executeRecaptcha) {
+        console.log("reCAPTCHA n'est pas encore chargé !");
+        return;
+    }
+
+    setSubmitting(true);
+    
+    // 1. Exécuter reCAPTCHA v3 pour obtenir le token
+    const token = await executeRecaptcha('contactFormSubmit'); // 'contactFormSubmit' est l'action
+
+    // 2. Récupérer les données du formulaire
+    const form = event.currentTarget as HTMLFormElement;
+    const formData = new FormData(form);
+    
+    // Ajouter le token reCAPTCHA aux données du formulaire pour Formspree
+    formData.append('g-recaptcha-response', token); 
+    
+    try {
+        // 3. Envoyer les données à Formspree
+        const response = await fetch(formSparkUrl, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json',
+            }
+        });
+
+        if (response.ok) {
+            alert("Message envoyé !");
+            form.reset();
+        } else {
+            alert("Erreur lors de l'envoi du message.");
+        }
+    } catch (error) {
+        console.error("Erreur lors de la soumission:", error);
+        alert("Erreur réseau ou interne lors de l'envoi.");
+    } finally {
+        setSubmitting(false);
+    }
+  };
+  return (
+    <div className="g-0 anchor">
+      <a href="/#contact" id="contact" aria-label="contact">
+        &nbsp;
+      </a>
+      <section className="contact__image">
+        <div className="contact__content">
+          <h3 className="h1 contact__title--color text-center text-uppercase p-5">
+            <span className="title"> Contact</span>
+          </h3>
+          <div className="d-flex flex-wrap-reverse justify-content-evenly">
+            <div className=" row g-0 mb-5 shadow border " id="lienContact">
+              <div className="m-0 g-0 p-0">
+                <div className="row p-3 align-items-center">
+                  <div className="text-center contact--color">
+                    <EmailIcon />
+                    <a
+                      className="nav-link underline__headerLinks nav__text--color   contact--color contact__content__text p-0 "
+                      href="mailto:lucaslengranddev@gmail.com"
+                      alt="Email"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      title="Email"
+                    >
+                      lucaslengranddev@gmail.com
+                    </a>
+                  </div>
+                </div>
+                <div className="row p-3 align-items-center">
+                  <div className="contact__icon text-center contact--color">
+                    <TwitterIcon />
+                    <a
+                      className="nav-link underline__headerLinks nav__text--color  contact--color p-0 contact__content__text "
+                      href="https://twitter.com/LucasLengrand2"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      title="Twitter"
+                    >
+                      lucaslengrand2
+                    </a>
+                  </div>
+                </div>
+                <div className="row p-3 align-items-center">
+                  <div className="contact__icon text-center contact--color">
+                    <EventAvailableIcon />
+                    <span className="nav-link underline__headerLinks nav__text--color contact--color p-0 contact__content__text ">
+                      <time dateTime="1996-08-24" title="Date de naissance">
+                        24/08/1996
+                      </time>
+                    </span>
+                  </div>
+                </div>
+                <div className="row p-3 align-items-center">
+                  <div className="contact__icon text-center contact--color">
+                    <GitHubIcon />
+                    <a
+                      className="nav-link underline__headerLinks nav__text--color contact--color contact__content__text p-0 "
+                      href="https://github.com/Melliaganz"
+                      target="_blank"
+                      rel="noreferrer"
+                      title="Github"
+                    >
+                      {" "}
+                      Melliaganz{" "}
+                    </a>
+                  </div>
+                </div>
+                <div className="row p-3 align-items-center">
+                  <div className="contact__icon text-center contact--color">
+                    <LinkedInIcon />
+                    <a
+                      className="nav-link underline__headerLinks nav__text--color contact--color  contact__content__text p-0"
+                      href="http://www.linkedin.com/in/LucasLengrand"
+                      title="Linkedin"
+                    >
+                      Lucas Lengrand
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              className=" border shadow
+           mb-5"
+              id="cardFormulaire"
+            >
+              <div className=" card-header">
+                Contactez moi ! <SendIcon />
+              </div>
+              <div className="card-body">
+                <form
+                  action={formSparkUrl}
+                  onSubmit={submitForm}
+                  data-netlify-recaptcha="true"
+                  data-netlify="true"
+                  className="row justify-content-center align-items-start"
+                  id="formulaire"
+                >
+                  <div className="row">
+                    <div className="col">
+                      <label htmlFor="name" className="form-label ">
+                        Nom et Prénom :{" "}
+                      </label>
+                      <div className="col">
+                        <input
+                          type="text"
+                          id="name"
+                          name="name"
+                          placeholder="Exemple: Marc Dupont"
+                          className="shadow form-control"
+                          required={true}
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <label htmlFor="email" className="form-label">
+                        Email :
+                      </label>
+                      <div className="col">
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          placeholder="Exemple: MarcDupont@gmail.com"
+                          className="shadow form-control"
+                          required={true}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col p-2 mt-4" id="messageForm">
+                    <label htmlFor="message" className="form-label ms-3">
+                      Message :
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      placeholder="Votre message ici !"
+                      required={true}
+                      rows="3"
+                      className="form-control shadow"
+                    ></textarea>
+                  </div>
+                  <input
+                    type="hidden"
+                    name="_redirect"
+                    value="https://www.lengrandlucas.com/#contact"
+                  />
+               <p className="mt-2 text-center text-muted small">
+                    Protégé par reCAPTCHA. <a href="https://www.google.com/intl/fr/policies/privacy/">Confidentialité</a> et <a href="https://www.google.com/intl/fr/policies/terms/">Conditions</a>.
+                  </p>
+                  <button
+                    disabled={submitting}
+                    type="submit"
+                    className="btn btn-light col-4 mt-2 shadow"
+                  >
+                    {submitting ? "Envoi !" : "Envoyer !"}{" "}
+                    <span className="EmailIcon">
+                      <EmailIcon />
+                    </span>{" "}
+                    <span className="DraftsIcon">
+                      <DraftsIcon />
+                    </span>
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
